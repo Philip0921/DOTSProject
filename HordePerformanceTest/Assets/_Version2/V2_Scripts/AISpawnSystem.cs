688d84bd62ba6df3bd9b27be2b068d3bb66abca1
Philip <39885159+Philip0921@users.noreply.github.com>
2025-10-20 09:11:36 +0200

    Save

---
+++ HordePerformanceTest/Assets/_Version2/V2_Scripts/AISpawnSystem.cs
@@ -0,0 +1,11 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+[BurstCompile]
+public partial struct AISpawnSystem : ISystem
+{
+
+}
---
+++ HordePerformanceTest/Assets/_Version2/V2_Scripts/AIWanderSystem.cs
@@ -0,0 +1,12 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+[BurstCompile]
+
+public partial struct AIWanderSystem : ISystem
+{
+
+}
